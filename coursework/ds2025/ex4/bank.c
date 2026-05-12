940d3fceec9be61fe01792d0de8a8ebfd858cdf5
Wenshen Liu <2236455213@qq.com>
2026-05-12 13:30:02 +0800

    git commit

--- coursework/ds2025/ex4/bank.c
+++ coursework/ds2025/ex4/bank.c
@@ -3,81 +3,80 @@
 
 typedef struct {
     int id;
-    int arrive_time;
-    int duration;
-    int wait_time;
+    int service_time;
+    int arrival_time;
 } Customer;
-
-Customer queue[1000];
-int head = 0, tail = 0;
-int windows[5] = {0, 0, 0, 0, 0}; // 0-2对私, 3-4临时
-int current_window_count = 3;
-
-void adjust_windows_increase() {
-    int waiting_count = tail - head;
-    while (current_window_count < 5 && waiting_count >= 7 * current_window_count) {
-        current_window_count++;
-    }
-}
-
-void adjust_windows_decrease() {
-    int waiting_count = tail - head;
-    while (current_window_count > 3) {
-        if (waiting_count < 7 * (current_window_count - 1)) {
-            current_window_count--;
-        } else {
-            break;
-        }
-    }
-}
 
 int main() {
     int T;
     if (scanf("%d", &T) != 1) return 0;
 
-    int *new_customer_counts = (int *)malloc(T * sizeof(int));
-    for (int i = 0; i < T; i++) scanf("%d", &new_customer_counts[i]);
+    int* arrivals = (int*)malloc(T * sizeof(int));
+    int total_customers = 0;
+    for (int i = 0; i < T; i++) {
+        scanf("%d", &arrivals[i]);
+        total_customers += arrivals[i];
+    }
 
-    int customer_id_counter = 1;
-    Customer finished[1000];
-    int finished_count = 0;
-
-    for (int t = 0; t < T || head < tail; t++) {
-        // 1. 新客户到达
-        if (t < T) {
-            for (int j = 0; j < new_customer_counts[t]; j++) {
-                queue[tail].id = customer_id_counter++;
-                queue[tail].arrive_time = t;
-                scanf("%d", &queue[tail].duration);
-                tail++;
-            }
-            // 只有在获取新客户时（包括0人）调增窗口
-            adjust_windows_increase();
+    Customer* all_customers = (Customer*)malloc(total_customers * sizeof(Customer));
+    int idx = 0;
+    for (int i = 0; i < T; i++) {
+        for (int j = 0; j < arrivals[i]; j++) {
+            all_customers[idx].id = idx + 1;
+            scanf("%d", &all_customers[idx].service_time);
+            all_customers[idx].arrival_time = i + 1;
+            idx++;
         }
+    }
 
-        // 2. 窗口尝试处理客户
-        for (int i = 0; i < current_window_count; i++) {
-            if (windows[i] == 0 && head < tail) {
-                Customer *c = &queue[head++];
-                c->wait_time = t - c->arrive_time;
-                windows[i] = c->duration;
-                finished[finished_count++] = *c;
-                
-                // 每当有人去接受服务，等待人数减少，立刻尝试减少窗口
-                adjust_windows_decrease();
-            }
-        }
+    int windows[5] = {0};
+    int active = 3;
+    int head = 0;
+    int tail = 0;
+    int time = 1;
 
-        // 3. 时间流逝，窗口服务时间减1
+    while (head < total_customers || windows[0] > 0 || windows[1] > 0 || 
+           windows[2] > 0 || windows[3] > 0 || windows[4] > 0) {
+        
         for (int i = 0; i < 5; i++) {
-            if (windows[i] > 0) windows[i]--;
+            if (windows[i] > 0) {
+                windows[i]--;
+            }
         }
+
+        if (time <= T) {
+            int arr_count = arrivals[time - 1];
+            tail += arr_count; 
+            
+            int queue_len = tail - head;
+            while (active < 5 && (double)queue_len / active >= 7.0) {
+                active++;
+            }
+        }
+
+        int queue_len = tail - head;
+        int assigned_this_cycle = 0;
+        for (int i = 0; i < active; i++) {
+            if (windows[i] == 0 && queue_len > 0) {
+                Customer c = all_customers[head];
+                windows[i] = c.service_time;
+                printf("%d : %d\n", c.id, time - c.arrival_time);
+                head++;
+                queue_len--;
+                assigned_this_cycle = 1;
+            }
+        }
+
+        if (assigned_this_cycle) {
+            while (active > 3 && (double)queue_len / active < 7.0) {
+                active--;
+            }
+        }
+
+        time++;
     }
 
-    for (int i = 0; i < finished_count; i++) {
-        printf("%d : %d\n", finished[i].id, finished[i].wait_time);
-    }
-
-    free(new_customer_counts);
+    free(arrivals);
+    free(all_customers);
     return 0;
 }
